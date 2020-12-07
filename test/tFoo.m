75650fa5c76d6210eef10ce545793d04e136c6e5
viswalal <30716026+viswalal@users.noreply.github.com>
2020-12-07 20:17:49 +0530

    Update tFoo.m

--- test/tFoo.m
+++ test/tFoo.m
@@ -1,8 +1,9 @@
 classdef tFoo < matlab.unittest.TestCase
-methods(Test)
-function testFnCall(test)
-  %test code
-  res = sourceFunction();
-  test.verifyEqual(res,'hello');
-end
+  methods(Test)
+   function testFnCall(test)
+      %test code
+      res = sourceFunction();
+      test.verifyEqual(res,'hello');
+   end
+  end
 end
